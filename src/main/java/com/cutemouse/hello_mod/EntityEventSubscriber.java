27135d76a8eee982055dd48f4ea73b2c7635c4b7
zlw380 <496607959@qq.com>
2023-05-29 22:39:35 +0800

    事件测试

--- src/main/java/com/cutemouse/hello_mod/EntityEventSubscriber.java
+++ src/main/java/com/cutemouse/hello_mod/EntityEventSubscriber.java
@@ -2,6 +2,7 @@ package com.cutemouse.hello_mod;
 
 import net.minecraft.Util;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.EntityEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -9,6 +10,8 @@ import net.minecraftforge.fml.common.Mod;
 //实体事件处理类
 @Mod.EventBusSubscriber
 public class EntityEventSubscriber {
+
+    protected static boolean playerEnterWorld = false;
 
     //实体被创建事件
     //玩家进入世界、丢弃物品、使用刷怪蛋、加载生物均为实体生成事件
@@ -18,6 +21,18 @@ public class EntityEventSubscriber {
         if (!event.getEntity().level.isClientSide()){
 
             System.out.println("在服务端触发了实体生成事件。");
+            if (event.getEntity() instanceof Player){
+
+                playerEnterWorld = true;
+                //在玩家加入世界之前会先生成玩家实体，再生成在玩家附近的其他生物实体。
+                //但如果在生成玩家实体时调用event.getEntity获取玩家信息，会因为获取不到玩家名称而抛出空指针异常使玩家无法加入世界。
+                System.out.println("该实体是一位玩家。");
+            }else if (playerEnterWorld){
+                System.out.println("该实体不是一位玩家，信息为：" + event.getEntity() + "。");
+            }
+
+            //加载世界时无法获取实体对象信息，会发生错误
+            //System.out.println("在服务端触发了实体生成事件，实体信息为" + event.getEntity() + "。");
         }
 
         //event.getEntity().sendMessage(new TextComponent("生成的实体信息：" + event.getEntity()), Util.NIL_UUID);
