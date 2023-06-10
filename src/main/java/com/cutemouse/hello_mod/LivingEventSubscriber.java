b127190864e09d13789cbd7b351db39c217d4290
zlw380 <496607959@qq.com>
2023-06-10 13:32:16 +0800

    事件测试

--- src/main/java/com/cutemouse/hello_mod/LivingEventSubscriber.java
+++ src/main/java/com/cutemouse/hello_mod/LivingEventSubscriber.java
@@ -1,6 +1,10 @@
 package com.cutemouse.hello_mod;
 
+import net.minecraft.Util;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.entity.living.AnimalTameEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -27,6 +31,18 @@ public class LivingEventSubscriber {
         //event.modifyVisibility(mod)方法修改可见性后不能作用到全局作用域，仅在本对象中有效。
         //在针对怪物降低玩家可见度为0.07后，除非距离极近，否则怪物无法探测到玩家。
         //在针对怪物降低玩家可见度为0.0后，除非贴身，否则怪物无法探测到玩家。
+    }
 
+    //动物驯服事件
+    @SubscribeEvent
+    public static void AnimalTameSub(AnimalTameEvent event){
+
+        if (event.getTamer() != null){
+
+            event.getTamer().sendMessage(new TextComponent(
+                    "玩家" + event.getTamer().getDisplayName().getString() +
+                            "驯服了" + event.getAnimal().getDisplayName().getString()
+            ), Util.NIL_UUID);
+        }
     }
 }
