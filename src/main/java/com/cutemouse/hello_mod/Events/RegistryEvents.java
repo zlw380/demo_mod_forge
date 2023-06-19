c29055b14e255c9f04f06315ec97c38f1c78620d
zlw380 <496607959@qq.com>
2023-06-19 16:59:01 +0800

    物品添加测试。

--- src/main/java/com/cutemouse/hello_mod/Events/RegistryEvents.java
+++ src/main/java/com/cutemouse/hello_mod/Events/RegistryEvents.java
@@ -1,5 +1,10 @@
 package com.cutemouse.hello_mod.Events;
 
+import net.minecraftforge.fml.common.Mod;
+
 //把所有的物品、方块、流体等全部在该类内进行注册
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class RegistryEvents {
+
+
 }
