749bd044af2a2c74a2bb72dfa0ffd8e52dac9478
zlw380 <496607959@qq.com>
2023-06-19 19:29:41 +0800

    物品添加测试。

--- src/main/java/com/cutemouse/hello_mod/Events/RegistryEvents.java
+++ src/main/java/com/cutemouse/hello_mod/Events/RegistryEvents.java
@@ -25,15 +25,20 @@ public class RegistryEvents {
     public static void registerItems(final RegistryEvent.Register<Item> event){
 
         //统一注册物品列表中的物品
-        //针对每一个物品创建新的物品对象并赋给物品列表中对应的引用
+        //针对每一个物品创建新的物品对象并将其赋给物品列表中对应的引用
+        //同时可以设置物品的一些属性，比如最大堆叠数量等。
+
+        //这种注册物品的方法与之前为每个物品单独创建实体类并注册到DeferredRegister类常量ITEMS中的方法是等效的。
         event.getRegistry().registerAll(
                 ItemList.vegetableSoup =
-                        new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS).stacksTo(16)).setRegistryName(location("vegetableSoup")),
+                        new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS).stacksTo(16)).setRegistryName(location("vegetable_soup")),
                 ItemList.fruitCake =
-                        new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS).stacksTo(8)).setRegistryName(location("fruitCake")),
+                        new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS).stacksTo(8)).setRegistryName(location("fruit_cake")),
                 ItemList.fruitPizza =
-                        new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS).stacksTo(4)).setRegistryName(location("fruitPizza"))
+                        new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS).stacksTo(4)).setRegistryName(location("fruit_pizza"))
         );
+        //这里的注册名有正则表达式约束，不允许使用大写英文字母，仅允许使用小写字母a-z，数字0-9，字符/._-。
+        //Exception message: net.minecraft.ResourceLocationException: Non [a-z0-9/._-] character in path of location: hello_mod:vegetableSoup
     }
 
     private static ResourceLocation location(String name) {
