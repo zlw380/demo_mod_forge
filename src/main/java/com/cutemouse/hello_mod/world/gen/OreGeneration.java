ea5e104b10baf58fff86797059d6a73124557079
zlw380 <496607959@qq.com>
2023-06-24 22:37:15 +0800

    矿物生成测试。

---
+++ src/main/java/com/cutemouse/hello_mod/world/gen/OreGeneration.java
@@ -0,0 +1,11 @@
+package com.cutemouse.hello_mod.world.gen;
+
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+
+public class OreGeneration {
+
+    public static void generateOres(final BiomeLoadingEvent event){
+
+
+    }
+}
